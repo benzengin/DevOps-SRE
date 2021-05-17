9d6a63f3b9f8684e0ad032e4e1635f74e6d41432
Bayram Zengin <77877779+benzengin@users.noreply.github.com>
2021-05-17 12:03:56 +0200

    Create backup.sh

---
+++ BASH/BEGINNER-SCRIPTS/backup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Backing up required files
+
+LOGFILE="/home/$USER/log_file"
+BACKUP_LOC="/usr/bin/*"
+BACKUP_TARGET="/home/$USER/backup"
+
+echo "Creating backup directory" && mkdir $BACKUP_TARGET 2> /dev/null || echo "Directory already exists."
+
+echo "Copying Files" $$ cp -v $BACKUP_LOC $BACKUP_TARGET > LOGFILE 2>&1
+
+grep -i denied log_file | tail -n 2
+
+exit 127
